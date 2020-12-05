a29316a0c5d3353d1c4b5db84555aa02592c60cc
Blake <bleonard252@rocketmail.com>
2020-12-05 16:08:08 -0500

    Build Pangolin

--- board/dahliaos/post-build.sh
+++ board/dahliaos/post-build.sh
@@ -3,6 +3,36 @@
 set -e
 
 BOARD_DIR=$(dirname "$0")
+OWD=$PWD
+if [ -f "dahliaos-overlays/dahlia/pangolin_desktop/pangolin_desktop"]; then
+    echo Using custom Pangolin build. Skipping automatic build.
+else 
+    if [ -d "$BINARIES_DIR/pangolin_desktop/" ]; then
+        echo Pangolin folder already exists, updating it...
+        cd $BINARIES_DIR/pangolin_desktop
+        git pull
+    else
+        echo Pangolin folder does not exist, getting it...
+        cd $BINARIES_DIR
+        git clone https://github.com/dahlia-os/pangolin-desktop pangolin_desktop
+        cd pangolin_desktop
+    fi
+
+    if ! which flutter 2>/dev/null || echo FALSE ; then
+        echo Flutter available: building Pangolin
+        flutter config --enable-linux-desktop
+        flutter build linux
+        if [ -d "$TARGET_DIR/dahlia/pangolin_desktop" ]; then
+            echo Directory exists, continuing...
+        else
+            mkdir $TARGET_DIR/dahlia/pangolin_desktop
+        fi
+        cp -r build/linux/release/bundle/* $TARGET_DIR/dahlia/pangolin_desktop/
+    else
+        echo "WARNING: Flutter not available! Pangolin will not be included!"
+    fi
+fi
+cd $OWD
 
 # Detect boot strategy, EFI or BIOS
 if [ -f "$BINARIES_DIR/efi-part/startup.nsh" ]; then
