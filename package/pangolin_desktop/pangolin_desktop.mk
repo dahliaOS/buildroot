30362d07c0878db3cd8fb341dbc45ab603424c10
Noah Cain <nmcain@gmail.com>
2021-01-17 06:28:07 +0000

    add pangolin as package (WIP)

---
+++ package/pangolin_desktop/pangolin_desktop.mk
@@ -0,0 +1,10 @@
+################################################################################
+#
+# pangolin_desktop
+#
+################################################################################
+
+PANGOLIN_DESKTOP_VERSION = latest
+PANGOLIN_DESKTOP_SOURCE = master.zip
+PANGOLIN_DESKTOP_SITE = https://github.com/dahliaOS/pangolin-desktop/archive/
+
