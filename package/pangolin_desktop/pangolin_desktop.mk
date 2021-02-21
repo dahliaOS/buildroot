7088ee81e6d2ebdede14aa44d5272d357de77316
Noah Cain <nmcain@gmail.com>
2021-02-21 01:10:19 +0000

    Move to BTRFS and add Pangolin as a package

--- package/pangolin_desktop/pangolin_desktop.mk
+++ package/pangolin_desktop/pangolin_desktop.mk
@@ -5,6 +5,16 @@
 ################################################################################
 
 PANGOLIN_DESKTOP_VERSION = latest
-PANGOLIN_DESKTOP_SOURCE = master.zip
-PANGOLIN_DESKTOP_SITE = https://github.com/dahliaOS/pangolin-desktop/archive/
+PANGOLIN_DESKTOP_SOURCE = master.tar.gz
+PANGOLIN_DESKTOP_SITE = https://github.com/dahliaOS/pangolin-desktop/archive
+PANGOLIN_DESKTOP_INSTALL_STAGING = NO
 # help
+define PANGOLIN_DESKTOP_BUILD_CMDS
+	cd $(@D)&&flutter build linux --debug
+endef
+
+define PANGOLIN_DESKTOP_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/dahlia/pangolin_desktop
+	cp -r $(@D)/build/linux/debug/bundle/* $(TARGET_DIR)/dahlia/pangolin_desktop
+endef
+$(eval $(generic-package))
