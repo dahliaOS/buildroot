21903e6e42108168c2f1ad17f8a211b0419bb476
Noah Cain <nmcain@gmail.com>
2021-02-25 02:39:39 +0000

    x64 target

--- package/pangolin_desktop/pangolin_desktop.mk
+++ package/pangolin_desktop/pangolin_desktop.mk
@@ -10,11 +10,11 @@ PANGOLIN_DESKTOP_SITE = https://github.com/dahliaOS/pangolin-desktop/archive
 PANGOLIN_DESKTOP_INSTALL_STAGING = NO
 # help
 define PANGOLIN_DESKTOP_BUILD_CMDS
-	cd $(@D)&&flutter build linux --debug
+	cd $(@D)&&flutter build linux --debug --no-sound-null-safety --verbose
 endef
 
 define PANGOLIN_DESKTOP_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/dahlia/pangolin_desktop
-	cp -r $(@D)/build/linux/debug/bundle/* $(TARGET_DIR)/dahlia/pangolin_desktop
+	cp -r $(@D)/build/linux/x64/debug/bundle/* $(TARGET_DIR)/dahlia/pangolin_desktop
 endef
 $(eval $(generic-package))
