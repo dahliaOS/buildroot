7e288be2d943b26d09d0e6a4ac417b2d14fd180d
Noah Cain <nmcain@gmail.com>
2022-01-11 17:17:34 +0000

    fix pangolin build script

--- package/pangolin_desktop/pangolin_desktop.mk
+++ package/pangolin_desktop/pangolin_desktop.mk
@@ -3,14 +3,12 @@
 # pangolin_desktop
 #
 ################################################################################
-
-PANGOLIN_DESKTOP_VERSION = main 
-PANGOLIN_DESKTOP_SITE = https://github.com/dahliaOS/pangolin_desktop.git
-PANGOLIN_DESKTOP_GIT_SUBMODULES = YES
+PANGOLIN_DESKTOP_SITE = https://github.com/dahliaOS/pangolin_desktop/archive/refs/heads/main.tar.gz
+#PANGOLIN_DESKTOP_GIT_SUBMODULES = YES
 PANGOLIN_DESKTOP_INSTALL_STAGING = NO
 # help
 define PANGOLIN_DESKTOP_BUILD_CMDS
-	cd $(@D)&&flutter build linux --release --verbose
+	cd $(@D)&&git clone https://github.com/dahliaOS/pangolin_desktop/&&mv pangolin_desktop/* ./&&flutter build linux --release --verbose
 	#echo "pangolin_desktop is disabled and will need to be installed manually."
 endef
 
